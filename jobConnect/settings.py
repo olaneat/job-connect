19e94a955aba4ab95f237514968db4c553e763ee
Tosin Ayoola <tosinayoola0@gmail.com>
2022-02-05 10:45:55 +0100

    changes

--- jobConnect/settings.py
+++ jobConnect/settings.py
@@ -117,7 +117,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:4200"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:4200", ]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
